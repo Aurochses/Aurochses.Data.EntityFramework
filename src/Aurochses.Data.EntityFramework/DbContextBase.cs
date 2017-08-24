123d2ae56a0890d767470275b9c38a3a87531d06
Vladimir Rodchenko <vladimir@aurochses.com>
2017-08-24 12:16:08 +0300

    Initial code without tests

---
+++ src/Aurochses.Data.EntityFramework/DbContextBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+
+namespace Aurochses.Data.EntityFramework
+{
+    /// <summary>
+    /// Class DbContextBase.
+    /// </summary>
+    /// <seealso cref="System.Data.Entity.DbContext" />
+    public class DbContextBase<TContext> : DbContext
+        where TContext : DbContext
+    {
+        static DbContextBase()
+        {
+            Database.SetInitializer<TContext>(null);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbContextBase{TContext}"/> class.
+        /// </summary>
+        /// <param name="nameOrConnectionString">The name or connection string.</param>
+        /// <param name="schemaName">Name of the schema.</param>
+        public DbContextBase(string nameOrConnectionString, string schemaName)
+            : base(nameOrConnectionString)
+        {
+            SchemaName = schemaName;
+        }
+
+        /// <summary>
+        /// Database schema name
+        /// </summary>
+        public string SchemaName
+        {
+            get;
+        }
+    }
+}
---
+++ src/Aurochses.Data.EntityFramework/EntityNoneDatabaseGeneratedIdentifier.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aurochses.Data.EntityFramework
+{
+    /// <summary>
+    /// Entity with non database generated identifier for data layer.
+    /// </summary>
+    /// <typeparam name="TType">The type of the t type.</typeparam>
+    /// <seealso>
+    ///     <cref>Aurochses.Data.IEntity{TType}</cref>
+    /// </seealso>
+    public class EntityNoneDatabaseGeneratedIdentifier<TType> : Entity<TType>
+    {
+        private bool _isNew;
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public override TType Id { get; set; }
+
+        /// <summary>
+        /// Determines whether this instance is new.
+        /// </summary>
+        /// <returns><c>true</c> if this instance is new; otherwise, <c>false</c>.</returns>
+        public override bool IsNew()
+        {
+            return _isNew;
+        }
+
+        /// <summary>
+        /// Marks instance as new.
+        /// </summary>
+        public void MarkAsNew()
+        {
+            _isNew = true;
+        }
+    }
+}
